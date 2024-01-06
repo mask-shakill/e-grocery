04c53f0f65b6469d9d8d52385e7cd43405760d9e
mask-shakill <mask-shakill>
2024-01-07 00:35:53 +0600

    added commonbutton

---
+++ src/components/Button/CommonButton.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CommonButton = () => {
+  return (
+    <div>
+      <h1 className="bg-slate-400">Common button </h1>
+    </div>
+  );
+};
+
+export default CommonButton;
